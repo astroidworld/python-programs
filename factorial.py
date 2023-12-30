79fff872f2fe1adca9bfe7a18848f20ce462bf58
kahlan <kahlan.guha123456@gmail.com>
2023-12-30 18:04:59 +0530

    factorial program written

--- factorial.py
+++ factorial.py
@@ -1,1 +1,14 @@
 # program for factorial
+
+def facto(num):
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial = factorial*i
+        
+    return factorial    
+
+
+
+# driver program
+number = 4
+print(f"The factorial of {number} is : ", facto(number))
